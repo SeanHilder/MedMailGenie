19b41b856f6bc9d9d7c72538fed42cd056f38826
HaiNam2312 <hainamdh.2312@gmail.com>
2026-09-09 23:41:57 +1000

    changes from tsconfig.json

---
+++ extensions/dist/content.js
@@ -0,0 +1,1 @@
+console.log(`MedMailGenie content script loaded`);
--- extensions/vite.config.ts
+++ extensions/vite.config.ts
@@ -1,11 +1,32 @@
 import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
   build: {
     outDir: "dist",
     emptyOutDir: true,
+
     rollupOptions: {
-      input: "popup.html",
+      input: {
+        popup: "popup.html",
+        background: "src/background.ts",
+        content: "src/content.ts",
+      },
+
+      output: {
+        entryFileNames: "[name].js",
+      },
     },
   },
+
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: "manifest.json",
+          dest: ".",
+        },
+      ],
+    }),
+  ],
 });
